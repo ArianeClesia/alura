531e604acc89ba985a4adbc8a1207b2f493d9b86
ArianeClesia <142044889+ArianeClesia@users.noreply.github.com>
2024-10-24 11:49:23 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Música',
+    'Quantos naipes tem numa orquestra?',
+    'Quatro naipes'
+)
+
+criaCartao(
+    'Música',
+    'Quais são esses naipes?',
+    'Metais, madeiras, cordas e percussão'
+)
+
+criaCartao(
+    'Música',
+    'Quais são os tipos de orquestra?',
+    'Orquestra Sinfônica, Orquestra Filarmônica e Orquestra de Câmara'
+)
+
+criaCartao(
+    'Música',
+    'Qual a diferença de cada uma?',
+    'A de Câmara coistuma ser constítuida por apenas um naipe (cordas, sopros ou percussão), a Sinfônica e a Filarmônica são compostas por todos os naipes a única diferença é como elas são mantidas financeiramente'
+)
+
+criaCartao(
+    'Música',
+    'A orquestra segue comandos de quem?',
+    'Elas seguem comando de um maestro/regente'
+)
+
+criaCartao(
+    'Música',
+    'Quais sãos as calves que nós utilizamos?',
+    'Clave de Sol, Fá e Dó'
+)
